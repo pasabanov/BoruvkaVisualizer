3cac5cfa93cc505fe2a85ea35b0b1b6271f33911
C00LNICK <30545094+C00LNICK@users.noreply.github.com>
2022-06-28 12:08:35 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.leti.summer_practice {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.leti.summer_practice to javafx.fxml;
+    exports com.leti.summer_practice;
+}
