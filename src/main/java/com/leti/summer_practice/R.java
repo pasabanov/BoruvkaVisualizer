9e19bb1d964437e9ae6aed81c1c167abaef03298
C00LNICK <30545094+C00LNICK@users.noreply.github.com>
2022-06-30 17:03:33 +0300

    Improved R. Deleted unnecessary classes.

--- src/main/java/com/leti/summer_practice/R.java
+++ src/main/java/com/leti/summer_practice/R.java
@@ -10,41 +10,102 @@ import java.util.ResourceBundle;
 
 public final class R {
 
-    private static ResourceBundle strings = ResourceBundle.getBundle("com/leti/summer_practice/strings");
+    private static final String BUNDLES_PATH = "com/leti/summer_practice/";
+    private static final String MAIN_STRINGS_BUNDLE_NAME = "strings";
+    private static final String MAIN_STRINGS_BUNDLE_PATH = BUNDLES_PATH + MAIN_STRINGS_BUNDLE_NAME;
+
+    private static ResourceBundle mainStringsBundle = ResourceBundle.getBundle(MAIN_STRINGS_BUNDLE_PATH);
+
 
     private R() {
         throw new Error("Forbidden instance of " + R.class.getName() + "!");
     }
 
-    public static ResourceBundle getResourceBundle() {
-        return strings;
+
+    /**
+     * @return main strings bundle.
+     */
+    public static ResourceBundle getMainStringsBundle() {
+        return mainStringsBundle;
     }
 
+    /**
+     * Get current Locale of the main strings bundle.
+     *
+     * @return locale of the bundle.
+     */
     public static Locale getLocale() {
-        return strings.getLocale();
+        return mainStringsBundle.getLocale();
     }
 
+    /**
+     * Sets Locale for the main strings bundle.
+     * (Creates new bundle with specified locale)
+     *
+     * @param locale - required locale
+     */
     public static void setLocale(Locale locale) {
-        strings = ResourceBundle.getBundle("com/leti/summer_practice/strings", locale);
+        mainStringsBundle = ResourceBundle.getBundle(MAIN_STRINGS_BUNDLE_PATH, locale);
     }
 
+    /**
+     * Get string from main strings bundle.
+     *
+     * @param key - name (id) of the required string.
+     * @return required string from bundle.
+     */
     public static String string(String key) {
-        return strings.getString(key);
+        return mainStringsBundle.getString(key);
     }
 
+    /**
+     * Get URL to resource by name.
+     *
+     * @param name - name of resource.
+     * @return URL to resource.
+     */
     public static URL resource(String name) {
         return R.class.getResource(name);
     }
 
+
+    /**
+     * Get FXMLLoader instance from a string or url with (or with not) specified bundle.
+     *
+     * @param url (string) - name of the .fxml file.
+     * @return FXMLLoader instance.
+     */
     public static FXMLLoader fxmlLoader(URL url) {
-        return new FXMLLoader(url, getResourceBundle());
+        return fxmlLoader(url, getMainStringsBundle());
     }
-
+    public static FXMLLoader fxmlLoader(URL url, ResourceBundle resourceBundle) {
+        return new FXMLLoader(url, resourceBundle);
+    }
     public static FXMLLoader fxmlLoader(String resourceName) {
-        return fxmlLoader(resource(resourceName));
+        return fxmlLoader(resourceName, getMainStringsBundle());
+    }
+    public static FXMLLoader fxmlLoader(String resourceName, ResourceBundle resourceBundle) {
+        return fxmlLoader(resource(resourceName), resourceBundle);
     }
 
+
+    /**
+     * Load a Parent (View) from an .fxml file by a url or filename with (or with not) specified bundle).
+     *
+     * @param url (string) - name of the .fxml file.
+     * @return a Parent instance.
+     * @throws IOException if parsing was unsuccessful.
+     */
+    public static Parent load(URL url) throws IOException {
+        return load(url, getMainStringsBundle());
+    }
+    public static Parent load(URL url, ResourceBundle resourceBundle) throws IOException {
+        return fxmlLoader(url, resourceBundle).load();
+    }
     public static Parent load(String resourceName) throws IOException {
-        return fxmlLoader(resourceName).load();
+        return load(resourceName, getMainStringsBundle());
+    }
+    public static Parent load(String resourceName, ResourceBundle resourceBundle) throws IOException {
+        return fxmlLoader(resourceName, resourceBundle).load();
     }
 }
---
+++ src/main/java/com/leti/summer_practice/gui/prog/SummerPracticeApplication.java
@@ -0,0 +1,23 @@
+package com.leti.summer_practice.gui.prog;
+
+import com.leti.summer_practice.R;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SummerPracticeApplication extends Application {
+
+    protected Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        primaryStage.setTitle(R.string("AppName"));
+        primaryStage.setScene(new Scene(R.load("hello-view.fxml"), 320, 240));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
