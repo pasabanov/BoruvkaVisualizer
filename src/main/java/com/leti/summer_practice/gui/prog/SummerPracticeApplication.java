3d05ac4e82dd1922f3a9627b4bd3469ee221deaa
C00LNICK <30545094+C00LNICK@users.noreply.github.com>
2022-07-04 02:53:29 +0300

    Preparing VectorCanvas. Little changes in GraphCanvas and SummerPracticeController.

--- src/main/java/com/leti/summer_practice/gui/prog/SummerPracticeApplication.java
+++ src/main/java/com/leti/summer_practice/gui/prog/SummerPracticeApplication.java
@@ -5,8 +5,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Locale;
-
 public class SummerPracticeApplication extends Application {
 
     private static SummerPracticeApplication application;
