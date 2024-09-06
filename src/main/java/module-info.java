46dbf50e191e79c915000855639efb6e2820ad38
Petr <30545094+pasabanov@users.noreply.github.com>
2024-09-07 00:29:56 +0300

    Removed redundant dependencies, added configuration files (#9)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,6 @@ module com.borviz.boruvkavisualizer {
 
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
 
     exports com.borviz.boruvkavisualizer.logic;
 
